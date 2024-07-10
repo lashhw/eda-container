427d9766b381b3c67915f29db8cb815b293c769b
lashhw <71396086+lashhw@users.noreply.github.com>
2024-07-11 00:44:53 +0800

    add dnf package

--- init.sh
+++ init.sh
@@ -13,8 +13,8 @@ rm -rf /var/cache/dnf
 subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
-dnf install -y sudo zsh tcsh vim firefox iputils iproute redhat-lsb-core binutils gcc-c++ \
-  xorg-x11-xauth xorg-x11-fonts-* xorg-x11-utils dbus-x11 \
+dnf install -y sudo zsh tcsh vim firefox iputils iproute redhat-lsb-core binutils gcc-c++ git \
+  xorg-x11-xauth xorg-x11-fonts-* xorg-x11-utils dbus-x11 google-noto-sans-cjk-ttc-fonts \
   libXft libXScrnSaver libXi libXrandr libSM libpng12 libglvnd-glx libXcomposite pulseaudio-libs-glib2 graphite2 \
   ncurses-compat-libs glibc.i686 glibc-devel.i686 libnsl numactl-libs
 
