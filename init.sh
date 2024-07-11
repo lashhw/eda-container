040fe3ad5479ecd184a9bf57cdd116ce5395fbb2
lashhw <71396086+lashhw@users.noreply.github.com>
2024-07-11 16:22:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,10 +13,11 @@ rm -rf /var/cache/dnf
 subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
-dnf install -y sudo zsh tcsh vim firefox iputils iproute redhat-lsb-core binutils gcc-c++ git \
+dnf install -y sudo zsh tcsh vim firefox iputils iproute redhat-lsb-core binutils gcc-c++ git ksh \
   xorg-x11-xauth xorg-x11-fonts-* xorg-x11-utils dbus-x11 google-noto-sans-cjk-ttc-fonts \
   libXft libXScrnSaver libXi libXrandr libSM libpng12 libglvnd-glx libXcomposite pulseaudio-libs-glib2 graphite2 \
-  ncurses-compat-libs glibc.i686 glibc-devel.i686 libnsl numactl-libs
+  ncurses-compat-libs glibc.i686 glibc-devel.i686 libnsl numactl-libs \
+  libnsl.i686 libXp.i686 libXt.i686 libXtst.i686
 
 ln -s /cad /usr/cad
 
